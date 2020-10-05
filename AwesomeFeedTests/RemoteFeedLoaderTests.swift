2134e60e5d7901fb06eab1de2830d178cb0b76fc
Edgar Tamio Hirama <edhirama@gmail.com>
2020-10-04 22:23:09 -0300

    RemoteFeeadLoader does not request data from URL on creatio

---
+++ AwesomeFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  AwesomeFeedTests
+//
+//  Created by Edgar Hirama on 04/10/20.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+
+        XCTAssertNil(client.requestedURL)
+    }
+}
