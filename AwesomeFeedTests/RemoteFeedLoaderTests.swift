918107358c7882cb746558cb052f67d1a8097567
Edgar Tamio Hirama <edhirama@gmail.com>
2020-10-04 22:45:11 -0300

    Add factory method

--- AwesomeFeedTests/RemoteFeedLoaderTests.swift
+++ AwesomeFeedTests/RemoteFeedLoaderTests.swift
@@ -37,19 +37,21 @@ class HTTPClientSpy: HTTPClient {
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
 
         sut.load()
         XCTAssertEqual(client.requestedURL, url)
+    }
+
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        return (RemoteFeedLoader(url: url, client: client), client)
     }
 }
