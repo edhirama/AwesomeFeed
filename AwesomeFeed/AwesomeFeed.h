37ff917720c684eedd6d250de98daf62a38b2036
Edgar Tamio Hirama <edhirama@gmail.com>
2020-10-04 22:13:49 -0300

    Initial Commit

---
+++ AwesomeFeed/AwesomeFeed.h
@@ -0,0 +1,18 @@
+//
+//  AwesomeFeed.h
+//  AwesomeFeed
+//
+//  Created by Edgar Hirama on 04/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AwesomeFeed.
+FOUNDATION_EXPORT double AwesomeFeedVersionNumber;
+
+//! Project version string for AwesomeFeed.
+FOUNDATION_EXPORT const unsigned char AwesomeFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeFeed/PublicHeader.h>
+
+
