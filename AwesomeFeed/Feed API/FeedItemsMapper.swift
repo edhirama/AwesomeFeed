580af7de7702faef3f116d35d6a7e9610fee4fc5
Edgar Tamio Hirama <edhirama@gmail.com>
2020-10-12 21:00:16 -0300

    Move all data and response mapping to FeedItemsMapper

---
+++ AwesomeFeed/Feed API/FeedItemsMapper.swift
@@ -0,0 +1,35 @@
+//
+//  FeedItemsMapper.swift
+//  AwesomeFeed
+//
+//  Created by Edgar Hirama on 12/10/20.
+//
+
+import Foundation
+
+internal final class FeedItemsMapper {
+
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+
+        var item: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
+    private static var OK_200: Int { 200 }
+
+    internal static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else { return .failure(RemoteFeedLoader.Error.invalidData) }
+
+        return .success(root.items.map { $0.item })
+    }
+}
