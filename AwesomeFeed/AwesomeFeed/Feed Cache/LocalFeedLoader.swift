e486dd01623cfdbb598ad8686c1b2dde44a2b12f
Edgar Tamio Hirama <edhirama@gmail.com>
2021-02-26 13:26:33 -0300

    Extract local members into properties

--- AwesomeFeed/AwesomeFeed/Feed Cache/LocalFeedLoader.swift
+++ AwesomeFeed/AwesomeFeed/Feed Cache/LocalFeedLoader.swift
@@ -11,8 +11,13 @@ public final class LocalFeedLoader {
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
 
-    let store: FeedStore
-    let currentDate: () -> Date
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
+
+    private var maxCacheAgeInDays: Int {
+        return 7
+    }
 
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -45,7 +50,7 @@ public final class LocalFeedLoader {
 
     private func validate(_ timestamp: Date) -> Bool {
         let calendar = Calendar.init(identifier: .gregorian)
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else { return false }
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
         return currentDate() < maxCacheAge
     }
 
