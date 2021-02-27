8161a9c49a51c57c5008522881129cda54138d55
Edgar Tamio Hirama <edhirama@gmail.com>
2021-02-27 18:14:52 -0300

    Move `Codable` from the framework-agnostic `LocalFeedImage` type to the new framework-specific `CodableFeedImage` type

--- AwesomeFeed/AwesomeFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ AwesomeFeed/AwesomeFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,15 +9,38 @@ import XCTest
 import AwesomeFeed
 
 class CodableFeedStore {
+
+    private struct CodableFeedImage: Codable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let url: URL
+
+        init(_ image: LocalFeedImage) {
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
+
+        var local: LocalFeedImage {
+            return  LocalFeedImage(id: id, description: description, location: location, url: url)
+        }
+    }
+
     private struct Cache: Codable {
-        let feed: [LocalFeedImage]
+        let feed: [CodableFeedImage]
         let timestamp: Date
+
+        var localFeed: [LocalFeedImage] {
+            feed.map { $0.local }
+        }
     }
 
     private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
 
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping (Error?) -> Void) {
-        let encodedValues = try! JSONEncoder().encode(Cache(feed: feed, timestamp: timestamp))
+        let encodedValues = try! JSONEncoder().encode(Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp))
         try! encodedValues.write(to: storeURL)
         completion(nil)
     }
@@ -29,7 +52,7 @@ class CodableFeedStore {
 
         let decoder = JSONDecoder()
         let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+        completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
     }
 }
 
