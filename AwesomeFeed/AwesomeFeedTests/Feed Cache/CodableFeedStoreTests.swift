01b843d375f04657605c2f5fb7a74130a93d4c48
Edgar Tamio Hirama <edhirama@gmail.com>
2021-02-27 17:32:07 -0300

    Retrieve from empty cache returns empty result

---
+++ AwesomeFeed/AwesomeFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodableFeedStoreTests.swift
+//  AwesomeFeedTests
+//
+//  Created by Edgar Hirama on 27/02/21.
+//
+
+import XCTest
+import AwesomeFeed
+
+class CodableFeedStore {
+    func retrieve(completion: @escaping (RetrieveCachedFeedResult) -> Void) {
+        completion(.empty)
+    }
+}
+
+class CodableFeedStoreTests: XCTestCase {
+
+    func test_retrieve_fromEmptyCacheReturnsEmptyCache() {
+        let store = CodableFeedStore()
+        let exp = expectation(description: "Wait for retrieval")
+        store.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default: XCTFail("Expected empty result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
+
+}
